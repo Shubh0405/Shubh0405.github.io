93abe748bf8c97b93503727542e5561a9376a3e2
Shubh Gupta <shubhngupta04@gmail.com>
2020-08-14 23:12:42 +0530

    Code Completed

---
+++ card_slider/js/index.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+ $(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        }
+    });
+  });
+
+console.log('hello');
